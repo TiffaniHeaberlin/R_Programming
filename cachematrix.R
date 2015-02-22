056755fa929d27aec61789390fbc1276402b31a6
TiffaniHeaberlin <tiffani.heaberlin@unitypoint.org>
2015-02-21 21:31:50 -0600

    Initial round of updates
    Updated first try

---
+++ cachematrix.R
@@ -0,0 +1,36 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeVector <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(mm) m <<- mm
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+
+## Write a short comment describing this function
+
+cachemean <- function(x=matrix(), ...) {
+  m <- x$getinverse
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)%*%data
+
+  x$setinverse(m)
+  m
+}
+
